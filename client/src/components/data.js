664f78007ce26851885d4f202db7313d19afb616
Atul Tomar <124292388+Atultomar01@users.noreply.github.com>
2024-10-09 07:46:38 +0530

    first commit

---
+++ client/src/components/data.js
@@ -0,0 +1,22 @@
+// Data for the application
+
+let movies = [
+    'Godfather', 
+    'Spider-Man', 
+    'Harry Potter',
+    'Zindagi Na Milegi Dobara',
+    'Pushpa',
+    '3 idiots',
+    'Seven Samurai',
+    'The Batman'
+]
+let slots = [
+    '09:00 AM',
+    '02:00 PM', 
+    '04:00 PM', 
+    '10:00 PM']
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2']
+
+exports.movies = movies;
+exports.slots = slots;
+exports.seats = seats;
